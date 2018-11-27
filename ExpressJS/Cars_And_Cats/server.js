d50b957c7f214d09f130a43248f544a0e77f5225
Jason Wang <wang.jason.010@gmail.com>
2018-11-27 12:46:55 -0800

    cats and cars ejs assignment

--- ExpressJS/Cars_And_Cats/server.js
+++ ExpressJS/Cars_And_Cats/server.js
@@ -1,13 +1,20 @@
-// Load the express module and store it in the variable express (Where do you think this comes from?)
 var express = require("express");
-console.log("Let's find out what express is", express);
-// invoke express and store the result in the variable app
 var app = express();
-console.log("Let's find out what app is", app);
-// use app's get method and pass it the base route '/' and a callback
-app.use(express.static(__dirname + "/static"));
 
-// tell the express app to listen on port 8000, always put this at the end of your server.js file
+app.use(express.static(__dirname + "/static"));
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
+app.get("/cars", function(request, response) {
+  response.render("cars");
+});
+app.get("/cats", function(request, response) {
+  response.render("cats");
+});
+app.get("/cars/new", function(request, response) {
+  response.render("carForm");
+});
+
 app.listen(8000, function() {
   console.log("listening on port 8000");
 });
