13c7e55271f404a1752bf5dd8775ca1e9ae205fc
Jason Wang <wang.jason.010@gmail.com>
2018-12-03 11:01:23 -0800

    restful tasks continued w/ angular DOM manip

--- Angular/Restful_Task_API/public/src/app/app.component.ts
+++ Angular/Restful_Task_API/public/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { HttpService } from "./http.service";
 
 @Component({
@@ -6,8 +6,30 @@ import { HttpService } from "./http.service";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "MEAN";
+  tasks;
+  thirdTask;
 
   constructor(private _httpService: HttpService) {}
+  ngOnInit() {
+    this.getTasksFromService();
+    this.getOneTaskFromService();
+  }
+
+  getTasksFromService() {
+    let observable = this._httpService.getTasks();
+    observable.subscribe(data => {
+      console.log("Got our tasks!", data);
+      this.tasks = data["data"];
+    });
+  }
+
+  getOneTaskFromService() {
+    let observable = this._httpService.getOneTask("5c04708c3669f4376acbc332");
+    observable.subscribe(data => {
+      console.log("Got one task!", data);
+      this.thirdTask = data["data"];
+    });
+  }
 }
--- Angular/Restful_Task_API/public/src/app/http.service.ts
+++ Angular/Restful_Task_API/public/src/app/http.service.ts
@@ -5,20 +5,13 @@ import { HttpClient } from "@angular/common/http";
   providedIn: "root"
 })
 export class HttpService {
-  constructor(private _http: HttpClient) {
-    this.getTasks();
-    this.getOneTask("5c04708c3669f4376acbc332");
-  }
+  constructor(private _http: HttpClient) {}
 
   getTasks() {
-    let tempObservable = this._http.get("/tasks");
-
-    tempObservable.subscribe(data => console.log("Got our tasks!", data));
+    return this._http.get("/tasks");
   }
 
   getOneTask(id: string) {
-    let tempObservable = this._http.get("/tasks/" + id);
-
-    tempObservable.subscribe(data => console.log("Got one task!", data));
+    return this._http.get("/tasks/" + id);
   }
 }
